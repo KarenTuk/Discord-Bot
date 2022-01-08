d664e3ece718ec68dd1220c99765c94309307f01
Karen Tuk <karen.tuk@ryerson.ca>
2022-01-07 19:19:29 -0500

    first commit

---
+++ ahttp.py
@@ -0,0 +1,180 @@
+import aiohttp
+from io import BytesIO
+from typing import Union
+
+
+class AIOHTTP:
+    def __init__(self, timeout=10):
+        self.timeout = aiohttp.ClientTimeout(total=timeout)
+        self.session = aiohttp.ClientSession(timeout=self.timeout)
+        self.image_types = ["image/png", "image/jpg", "image/jpeg", "image/x-icon"]
+        self.media_types = self.image_types + ["image/gif"]
+
+    async def is_image(self, url: str) -> bool:
+        try:
+            async with self.session.head(url) as resp:
+                if resp.status == 200:
+                    mime = resp.headers.get("Content-type", "").lower()
+                    if any([mime == x for x in self.image_types]):
+                        return True
+                    else:
+                        return False
+        except:
+            return False
+
+    async def is_media(self, url: str) -> bool:
+        try:
+            async with self.session.head(url) as resp:
+                if resp.status == 200:
+                    mime = resp.headers.get("Content-type", "").lower()
+                    if any([mime == x for x in self.media_types]):
+                        return True
+                    else:
+                        return False
+        except:
+            return False
+
+    async def is_safe(self, url: str, max_bytes: int = 8388608) -> bool:
+        try:
+            async with self.session.head(url) as resp:
+                if resp.status == 200:
+                    tBytes = int(resp.headers.get("content-length", str(max_bytes + 1)))
+                    return tBytes <= max_bytes
+        except:
+            return False
+
+    async def is_gif(self, url: str) -> bool:
+        try:
+            async with self.session.head(url) as resp:
+                if resp.status == 200:
+                    mime = resp.headers.get("Content-type", "").lower()
+                    if mime == "image/gif":
+                        return True
+                    else:
+                        return False
+        except:
+            return False
+
+    async def download(self, url: str, path: str) -> None:
+        try:
+            async with self.session.get(url) as resp:
+                data = await resp.read()
+                with open(path, "wb") as f:
+                    f.write(data)
+        except:
+            return
+
+    async def bytes_download(
+        self, url: str, no_io: bool = False
+    ) -> Union[BytesIO, bytes]:
+        try:
+            async with self.session.get(url) as resp:
+                data = await resp.read()
+                if no_io:
+                    return data
+
+                return BytesIO(data)
+        except:
+            return
+
+    async def get_json(self, url: str, default=None) -> Union[dict, None]:
+        try:
+            async with self.session.get(url) as resp:
+                try:
+                    load = await resp.json()
+                    return load
+                except:
+                    return default
+        except:
+            return default
+
+    async def get_text(self, url: str, default=None) -> Union[str, None]:
+        try:
+            async with self.session.get(url) as resp:
+                try:
+                    text = await resp.text()
+                    return text
+                except:
+                    return default
+        except:
+            return default
+
+    async def get(self, url: str, **kwargs):
+        headers = kwargs.get("headers")
+        params = kwargs.get("params")
+        return_type = kwargs.get("return_type")
+        t = kwargs.get("timeout")
+        if t is not None:
+            timeout = aiohttp.ClientTimeout(total=t)
+        else:
+            timeout = self.timeout
+
+        try:
+            async with self.session.get(
+                url, headers=headers, params=params, timeout=timeout
+            ) as resp:
+                if return_type == "text":
+                    try:
+                        text = await resp.text()
+                        return text
+                    except:
+                        return None
+                elif return_type == "json":
+                    try:
+                        load = await resp.json()
+                        return load
+                    except:
+                        return None
+                elif return_type == "bytes":
+                    try:
+                        load = await resp.read()
+                        return BytesIO(load)
+                    except:
+                        return None
+                else:
+                    return resp
+        except:
+            return
+
+    async def post(self, url: str, **kwargs):
+        data = kwargs.get("data")
+        headers = kwargs.get("headers")
+        return_type = kwargs.get("return_type")
+        t = kwargs.get("timeout", 5)
+        timeout = aiohttp.ClientTimeout(total=t)
+        if t is not None:
+            timeout = aiohttp.ClientTimeout(total=t)
+        else:
+            timeout = self.timeout
+        try:
+            async with self.session.post(
+                url, data=data, headers=headers, timeout=timeout
+            ) as resp:
+                if return_type == "text":
+                    try:
+                        text = await resp.text()
+                        return text
+                    except:
+                        return None
+                elif return_type == "json":
+                    try:
+                        load = await resp.json()
+                        return load
+                    except:
+                        return None
+                elif return_type == "bytes":
+                    try:
+                        load = await resp.read()
+                        return BytesIO(load)
+                    except:
+                        return None
+                else:
+                    return resp
+        except:
+            return
+
+    async def close(self):
+        await self.session.close()
+
+
+requests = AIOHTTP()
---
+++ bot.py
@@ -0,0 +1,143 @@
+###############################
+'''this loads the "DISCORD_TOKEN" string from the .env file'''
+
+# import pip._vendor.certifi
+
+from urllib.parse import quote
+import random
+import helpers
+from ahttp import requests
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.environ["DISCORD_TOKEN"] #grabs the token after loading it with load_dotenv()
+
+###############################
+
+import discord
+
+from discord.ext import commands
+import datetime
+import json
+
+intents = discord.Intents.default() # will require all intents by default, change this to match your bot's intents
+
+bot = commands.Bot(
+                    command_prefix="?",
+                    intents=intents,
+                    help_command=None # <- uncomment this if you want to write your own help command
+                    )
+
+@bot.event # @bot.event attaches the function to the bot's event listener. 
+async def on_ready(): #the name of the function denotes which event it listens to
+    print(f"{bot.user.name} has connected to Discord!")
+
+@bot.event
+async def on_message(message : discord.Message):
+    # print(message.content)
+    if message.author == bot.user: #does not attempt to process any command called by the bot itself
+        return
+    if message.content == "Hi":
+        await message.channel.send("😎👍")
+    await bot.process_commands(message=message)
+
+@bot.command()
+async def pp(ctx):
+    """ Create a pp in discord """
+    pp_s = random.randint(0,100)
+    shaft = "=" * pp_s
+    await ctx.embed(title = f"8{shaft}D")
+
+@bot.command()
+async def clown(ctx: commands.Context, *, msg:str):
+    """ Alternative capitalization of characters in a sentence """
+    result = ""
+    for x in range(len(msg)):
+        if x%2==0 and msg[x].islower():
+            result += (msg[x].capitalize())
+        elif x%2==0 and msg[x].isupper():
+            result += (msg[x].lower())
+        else:
+            result += (msg[x])
+    await ctx.send(result) 
+
+@bot.command()
+async def cardi(ctx: commands.Context, *, msg:str):
+    """ Cardi-fy a sentence"""
+    okur = "U" * random.randint(1,10) + "R" * random.randint(1,10)
+    await ctx.send(f"{msg} OK{okur}")
+
+@bot.command()
+async def period(ctx: commands.Context, *, msg:str):
+    """ Add 'PERIODT' to the end of your message """
+    await ctx.send(f"{msg}. PERIODT 💅")
+
+@bot.command()
+async def sis(ctx: commands.Context):
+    await ctx.embed(title=f"🙌 Go off Sis 🙌 ")
+
+@bot.command()
+async def spread(ctx: commands.Context, *, msg:str):
+    """ Spread the characters in a sentence"""
+    result = ""
+    for x in range(len(msg)):
+        result+= " " + msg[x]
+    await ctx.send(result)
+
+@bot.command()
+async def wendys(ctx:commands.Context):
+    await ctx.embed(title="Ma'am, this is a Wendy's. ")
+
+@bot.command()
+async def pfp(ctx, member: discord.Member=None):
+    member = member or ctx.author
+    await ctx.embed(title=member, image_url=member.avatar_url)
+
+@bot.command()
+async def info(ctx, member: discord.Member=None):
+    member = member or ctx.author
+    await ctx.embed(title="Member info", description = f"Display name: {member.display_name}\nMember id: {member.id}\nDate created: {member.created_at.strftime('%A, %d %B %Y')}")
+
+@bot.command()
+async def date(ctx:commands.Context):
+    await ctx.embed(title="Today's Date", description = datetime.datetime.today().strftime("%A, %d %B %Y"))
+
+
+@bot.command()
+async def kanye(ctx:commands.Context):
+    data = await requests.get_json("https://api.kanye.rest/")
+    if data is None:
+        await ctx.embed(title = "Could not get quote")
+    await ctx.embed(title = "Kanye West Quote", description = data["quote"])
+
+
+@bot.command()
+async def bored(ctx:commands.Context):
+    data = await requests.get_json("http://www.boredapi.com/api/activity/")
+    if data is None:
+        await ctx.embed(title = "could not fulfill request")
+    await ctx.embed(title="Random Activity", description = f'Activity: {data["activity"]}\nType: {data["type"]}\nParticipants: {data["participants"]}\nPrice: {data["price"]}\nAccesibilty Rating: {data["accessibility"]}')
+
+@bot.command()
+async def fox(ctx:commands.Context):
+    data = await requests.get_json("https://randomfox.ca/floof/?ref=apilist.fun")
+    if data is None:
+        await ctx.embed(title = "could not fulfill request")
+    await ctx.embed(title = "Random Fox Picture", image_url= data["image"])
+
+
+@bot.command()
+async def google(ctx:commands.Context, *, query: commands.clean_content):
+    data = await requests.get_json(f"https://gurgle.nathaniel-fernandes.workers.dev/?q={quote(query)}", [])
+
+    if len(data) == 0:
+        return await ctx.embed(title = "could not fulfill request")
+    await ctx.embed(image_url = data[0], footer={"text": f"search term: {query}"}, color=0x2F3136)
+
+
+
+
+bot.run(TOKEN) #runs a bot with the specified [TOKEN], this connects the wrapper to your bot 
